a3f8681cc229ed3fc94cd67b9f12a0d6c237c8f7
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-08-27 18:23:45 +0530

    Update FormA.js

--- src/components/FormA.js
+++ src/components/FormA.js
@@ -1,19 +1,33 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import Summary from "./Summary";
 
 function FormA({ onSubmit, age }) {
+    const [show, setShow] = useState("");
+    const [formData, setFormData] = useState("");
+
     const handleSubmit = (event) => {
+        event.preventDefault();
+        if (show != "") {
+            setFormData({
+                type: "Form A",
+                age: age,
+                show: show,
+            });
+        }
     };
-    return (
+    return formData === "" ? (
         <form id="dc" onSubmit={handleSubmit}>
             <h2>Form A</h2>
             <label>
                 Select DC Shows:
-                <select  >
+                <select onChange={(e) => setShow(e.target.value)}>
                     <option value="">--Select--</option>
                     <option value="The Flash">The Flash</option>
                     <option value="Arrow">Arrow</option>
                     <option value="Supergirl">Supergirl</option>
-                    <option value="Legends of Tomorrow">Legends of Tomorrow</option>
+                    <option value="Legends of Tomorrow">
+                        Legends of Tomorrow
+                    </option>
                 </select>
             </label>
             <br />
@@ -22,9 +36,12 @@ function FormA({ onSubmit, age }) {
                 <input type="number" value={age} disabled />
             </label>
             <br />
-            <button id="submit-dc" type="submit">Submit</button>
-
+            <button id="submit-dc" type="submit">
+                Submit
+            </button>
         </form>
+    ) : (
+        <Summary formData={formData} />
     );
 }
 export default FormA;
