8912daa0a3fea7246ddff47f688d546a3a1e4c8f
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-08-27 18:25:08 +0530

    Update Summary.js

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,15 +1,23 @@
-import { useState } from "react";
+import React, { useState } from "react";
 function Summary({ formData }) {
     return (
         <div id="summary">
             <h2>Summary</h2>
-            <p>Form Type: </p>
-            <p>Age: </p>
+            <p>Form Type: {formData.type}</p>
+            <p>Age: {formData.age}</p>
 
-            <p>DC Shows: </p>
+            {formData.type === "FormA" ? (
+                <p>DC Shows: {formData.show}</p>
+            ) : (
+                <p>Marvel Shows: {formData.show}</p>
+            )}
 
-            <p>Marvel Shows: </p>
-
+            <button
+                id="start-over"
+                onClick={() => window.location.reload(true)}
+            >
+                Start Over
+            </button>
         </div>
     );
 }
